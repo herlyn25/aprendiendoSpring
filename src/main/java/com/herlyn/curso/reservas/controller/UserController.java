d340d4703579d77b9d894e556fca3d14f9b16580
hcastillo <hcastillo@lsv-tech.com>
2025-03-22 10:40:52 -0500

    Practicando Lista de objetos y el ciclo each

--- src/main/java/com/herlyn/curso/reservas/controller/UserController.java
+++ src/main/java/com/herlyn/curso/reservas/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.herlyn.curso.reservas.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.herlyn.curso.reservas.model.dto.UserDtoDetail;
 
 @Controller
 public class UserController {
@@ -14,6 +20,18 @@ public class UserController {
         model.put("lastname", "Castillo Martinez");
         //model.put("email", "herly.sicte@gmail.com");
         return "details";    
+    }
+
+    @GetMapping("/listmap")
+    public String listmap(ModelMap model){
+        
+        UserDtoDetail userdto= new UserDtoDetail("Erlin", "Castillo");
+        UserDtoDetail userdto2= new UserDtoDetail("Elkin", "Castillo");
+        UserDtoDetail userdto3= new UserDtoDetail("Elver", "Castillo"); 
+        List<UserDtoDetail> users = Arrays.asList(userdto,userdto2,userdto3);
+        model.addAttribute("users",users);
+        model.addAttribute("title","Listado de usuarios");
+        return "list";
     }
 
 }
