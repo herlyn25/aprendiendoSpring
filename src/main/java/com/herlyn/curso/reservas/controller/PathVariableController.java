5b6a213c6e0111a2a55a431effbedd8618ad5d92
hcastillo <hcastillo@lsv-tech.com>
2025-03-22 23:14:44 -0500

    Enviando una peticion Post

--- src/main/java/com/herlyn/curso/reservas/controller/PathVariableController.java
+++ src/main/java/com/herlyn/curso/reservas/controller/PathVariableController.java
@@ -7,6 +7,9 @@ import com.herlyn.curso.reservas.model.dto.UserDtoDetail;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -16,5 +19,13 @@ public class PathVariableController {
     @GetMapping("/user/{name}/{lastname}")
     public UserDtoDetail getMethodName(@PathVariable String name, @PathVariable String lastname) {
         return new UserDtoDetail(name.substring(0,1).toUpperCase()+name.substring(1),lastname);
-    }    
+    }
+    
+    @PostMapping("/create")
+    public UserDtoDetail postMethodName(@RequestBody UserDtoDetail entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+    
 }
